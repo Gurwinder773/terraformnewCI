21823a7e9d6558a4e5d03ef0c40dc4fde0512600
LohitaSharma <67069747+LohitaSharma@users.noreply.github.com>
2023-10-17 20:42:37 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 # Service Principal Variables
 
 variable "client_id" {
-    description =   "89d2feb5-8358-4c67-8693-f3e5c97f4cd0"
+    description =   "0b463b4a-2371-4daa-95eb-2aec1416718c"
     type        =   string
 }
 
 variable "client_secret" {
-    description =   ".BF8Q~etJ~dbuHD2JJemkqFbor.T5CQNCHnh1ail"
+    description =   "uXC8Q~kybEZ1Ta8BJD2oG9IvRaXV6FsfSU23cbm4"
     type        =   string
 }
 
 variable "subscription_id" {
-    description =   "321f31e2-7fb6-48d9-aa03-234a4ecaf9ca"
+    description =   "89f255bf-33e7-4aa7-a855-f95bd174329d"
     type        =   string
 }
 
 variable "tenant_id" {
-    description =   "3d83ab06-3c39-47e5-8d08-533b0bfdec20"
+    description =   "95e72c78-d792-406a-b53d-89192dc23de3"
     type        =   string
 }
