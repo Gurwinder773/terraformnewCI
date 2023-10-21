f0179470407399a1310d397ac1d60558c2add614
LohitaSharma <67069747+LohitaSharma@users.noreply.github.com>
2023-10-20 20:43:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,9 +101,9 @@ resource "azurerm_virtual_machine" "main" {
     managed_disk_type = "Standard_LRS"
   }
   os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password12345"
+    computer_name  = "Linux (ubuntu 22.04)"
+    admin_username = "lohita"
+    admin_password = "Littlehearts79"
   }
   os_profile_linux_config {
     disable_password_authentication = false
